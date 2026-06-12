d6fdc6c662487b6441c4d04572f7811f3c8a24ed
nivedhaasokan6-coder <nivedhaasokan6@gmail.com>
2026-06-12 14:12:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_file
 import pandas as pd
 import joblib
 
@@ -15,11 +15,7 @@ le = joblib.load("label_encoder.pkl")
 # STEP 1 - Collect Dataset
 @app.route("/")
 def home():
-    rows = len(df)
-    cols = len(df.columns)
-    return render_template("index.html",
-                           rows=rows,
-                           cols=cols)
+    return send_file("index.html")
 
 
 # STEP 2 - Preprocess Data
@@ -28,8 +24,10 @@ def preprocess():
 
     missing = df.isnull().sum().sum()
 
-    return render_template("preprocess.html",
-                           missing=missing)
+    return render_template(
+        "preprocess.html",
+        missing=missing
+    )
 
 
 # STEP 3 - Analyze Student Activities
@@ -39,9 +37,11 @@ def analysis():
     login_avg = round(df["Login_Frequency"].mean(), 2)
     quiz_avg = round(df["Quiz_Performance_Average"].mean(), 2)
 
-    return render_template("analysis.html",
-                           login_avg=login_avg,
-                           quiz_avg=quiz_avg)
+    return render_template(
+        "analysis.html",
+        login_avg=login_avg,
+        quiz_avg=quiz_avg
+    )
 
 
 # STEP 4 - Predict Engagement Page
